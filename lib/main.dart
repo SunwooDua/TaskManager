0e74ed032b5ef3b18a576957a54cb0c82ae4ec72
Sunwoo Yeom <o55306605@gmail.com>
2025-02-25 16:31:36 -0500

    add, checkbox, and input function is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TaskListScreen());
+  runApp(TaskManager());
+}
+
+class TaskManager extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: TaskListScreen());
+  }
 }
 
 class TaskListScreen extends StatefulWidget {
@@ -12,8 +19,56 @@ class TaskListScreen extends StatefulWidget {
 }
 
 class _TaskListScreenState extends State<TaskListScreen> {
+  final List<Map<String, dynamic>> tasks = [];
+  final TextEditingController taskController = TextEditingController();
+  bool? _isChecked = false;
+
+  void addTask() {
+    if (taskController.text.isNotEmpty) {
+      setState(() {
+        tasks.add({'text': taskController.text, 'isChecked': false});
+        taskController.clear();
+      });
+    }
+  }
+
+  void toggleTask(int index, bool? newValue) {
+    setState(() {
+      tasks[index]['isChecked'] = newValue ?? false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('TaskList')),
+      body: Column(
+        children: [
+          TextField(
+            controller: taskController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Enter Tasks",
+              suffixIcon: ElevatedButton(
+                onPressed: addTask,
+                child: Text('Add'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(tasks[index]['text']),
+                  value: tasks[index]['isChecked'],
+                  onChanged: (newValue) => toggleTask(index, newValue),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
